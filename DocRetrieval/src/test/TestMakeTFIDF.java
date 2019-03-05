3d744cd5d0bdf084d6ff5bae52cdf4d06c279d6c
xanadila <>
2019-03-05 22:24:01 +0700

    update test

--- DocRetrieval/src/test/TestMakeTFIDF.java
+++ DocRetrieval/src/test/TestMakeTFIDF.java
@@ -5,45 +5,37 @@
  */
 package test;
 
+/**
+ *
+ * @author xanadila
+ */
 import java.util.ArrayList;
-import java.util.List;
 import model.Document;
 import model.InvertedIndex;
 import model.Posting;
 import model.Term;
 
-/**
- *
- * @author admin
- */
-public class testTFIDF3 {
-
+public class TestMakeTFIDF {
     public static void main(String[] args) {
-        // seting dokumen
+         // seting dokumen
         Document doc1 = new Document(1, "Shipment of gold damaged in a fire");
         Document doc2 = new Document(2, "delivery of silver arrived in a silver truck");
         Document doc3 = new Document(3, "shipment of gold arrived in a truck");
-
+        
         // buat object invertedIndex
         InvertedIndex index = new InvertedIndex();
         // tmbahkan document ke index
         index.addNewDocument(doc1);
         index.addNewDocument(doc2);
         index.addNewDocument(doc3);
-
+        
         // panggil fungsi search
         index.makeDictionaryWithTermNumber();
-        ArrayList<Term> result = index.getDictionary();
-        // tampilkan isi document dan id-nya
-        for (int i = 0; i < result.size(); i++) {
-            System.out.println("Term = " + result.get(i).getTerm()
-                    + ",numberOfDocument = " + result.get(i).getNumberOfDocument());
-            ArrayList<Posting> tempPosting = result.get(i).getPostingList();
-            for (int j = 0; j < tempPosting.size(); j++) {
-                System.out.println("id_doc = " + tempPosting.get(j).getDocument().getId()
-                        + ", numberofTerm = " + tempPosting.get(j).getNumberOfTerm());
-            }
+        
+        ArrayList<Posting> postings = index.makeTFIDF(2);
+        
+        for (int i = 0; i < postings.size(); i++) {
+            System.out.println(postings.get(i).getTerm() + " : " + postings.get(i).getWeight());
         }
-
     }
 }
--- DocRetrieval/src/test/TestTfIdf.java
+++ DocRetrieval/src/test/TestTfIdf.java
@@ -6,7 +6,6 @@
 package test;
 
 import java.util.ArrayList;
-import java.util.List;
 import model.Document;
 import model.InvertedIndex;
 import model.Posting;
@@ -14,46 +13,51 @@ import model.Term;
 
 /**
  *
- * @author admin
+ * @author xanadila
  */
-public class testTFIDF5 {
-
+public class TestTfIdf {
     public static void main(String[] args) {
         // seting dokumen
         Document doc1 = new Document(1, "Shipment of gold damaged in a fire");
         Document doc2 = new Document(2, "delivery of silver arrived in a silver truck");
         Document doc3 = new Document(3, "shipment of gold arrived in a truck");
-
+        
         // buat object invertedIndex
         InvertedIndex index = new InvertedIndex();
         // tmbahkan document ke index
         index.addNewDocument(doc1);
         index.addNewDocument(doc2);
         index.addNewDocument(doc3);
-
+        
         // panggil fungsi search
         index.makeDictionaryWithTermNumber();
         ArrayList<Term> result = index.getDictionary();
         // tampilkan isi document dan id-nya
         for (int i = 0; i < result.size(); i++) {
-            System.out.println("Term = " + result.get(i).getTerm()
-                    + ",numberOfDocument = " + result.get(i).getNumberOfDocument());
-            ArrayList<Posting> tempPosting = result.get(i).getPostingList();
+            System.out.println("Term = "+result.get(i).getTerm()+
+                    ",numberOfDocument = "+result.get(i).getNumberOfDocument());
+            ArrayList<Posting> tempPosting = result.get(i).getTermList().getPostings();
             for (int j = 0; j < tempPosting.size(); j++) {
-                System.out.println("id_doc = " + tempPosting.get(j).getDocument().getId()
-                        + ", numberofTerm = " + tempPosting.get(j).getNumberOfTerm());
+                System.out.println("id_doc = "+tempPosting.get(j).getDocument().getId()
+                +", numberofTerm = "+tempPosting.get(j).getNumberOfTerm());
             }
         }
-
+        
         // number of document
         String tempString = "silver";
         int result2 = index.getDocumentFrequency(tempString);
-        System.out.println("Number of Doc with " + tempString + " is " + result2);
-
+        System.out.println("Number of Doc with "+tempString+" is "+result2);
+        
         // idf
-        String tempString1 = "silver";
-        double result3 = index.getDocumentFrequency(tempString1);
-        System.out.println("IDF of " + tempString1 + " is " + result3);
-
+        String tempString1 = "arrived";
+        double result3 = index.getInverseDocumentFrequency(tempString1);
+        System.out.println("IDF of "+tempString1+" is "+result3);
+        
+        // tf
+        // idf
+        String tempString2 = "fire";
+        int idDoc=1;
+        int result4 = index.getTermFrequency(tempString2, idDoc);
+        System.out.println("TF of "+tempString2+" in idDoc ="+idDoc+ " is "+result4);       
     }
 }
