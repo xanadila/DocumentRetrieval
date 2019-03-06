f78bb8b45331b33efef588bc798fbc3e7e869818
xanadila <>
2019-03-06 10:53:50 +0700

    test v2.0

---
+++ DocRetrieval/src/test/testDocument6.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import model.Document;
+import model.InvertedIndex;
+import model.Posting;
+import model.Term;
+
+/**
+ *
+ * @author admin
+ */
+public class testDocument6 {
+    public static void main(String[] args) {
+        // seting dokumen
+        Document doc1 = new Document(1, "computer information retrieval.");
+        Document doc2 = new Document(2, "computer organization and architecture");
+        Document doc3 = new Document(3, "machine learning architecture");
+
+        // buat object invertedIndex
+        InvertedIndex index = new InvertedIndex();
+        // tmbahkan document ke index
+        index.addNewDocument(doc1);
+        index.addNewDocument(doc2);
+        index.addNewDocument(doc3);
+        index.makeDictionary();
+        for (int i = 0; i < index.getDictionary().size(); i++) {
+            Term tempTerm = index.getDictionary().get(i);
+            System.out.println(tempTerm.getTerm() + "," + tempTerm.getNumberOfDocument());
+            for (int j = 0; j < tempTerm.getNumberOfDocument(); j++) {
+                Posting tempPosting = tempTerm.getPostingList().get(j);
+                Document tempDoc = tempPosting.getDocument();
+                System.out.println(" idDoc = " + tempDoc.getId());
+            }
+        }
+
+}
+}
---
+++ DocRetrieval/src/test/testDocument7.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import java.util.ArrayList;
+import model.Document;
+import model.InvertedIndex;
+import model.Posting;
+
+/**
+ *
+ * @author admin
+ */
+public class testDocument7 {
+
+    public static void main(String[] args) {
+        // seting dokumen
+        Document doc1 = new Document(1, "computer information retrieval.");
+        Document doc2 = new Document(2, "computer organization and architecture");
+        Document doc3 = new Document(3, "machine learning architecture");
+
+        // buat object invertedIndex
+        InvertedIndex index = new InvertedIndex();
+        // tmbahkan document ke index
+        index.addNewDocument(doc1);
+        index.addNewDocument(doc2);
+        index.addNewDocument(doc3);
+        index.makeDictionary();
+        // panggil fungsi search
+        ArrayList<Posting> result = index.searchOneWord("machine");
+        // tampilkan isi document dan id-nya
+        for (int i = 0; i < result.size(); i++) {
+            System.out.println("id_doc = " + result.get(i).getDocument().getId());
+            System.out.println(result.get(i).getDocument().getContent());
+        }
+
+//         panggil fungsi search
+        ArrayList<Posting> result1 = index.searchOneWord("organization");
+        // tampilkan isi document dan id-nya
+        for (int i = 0; i < result1.size(); i++) {
+            System.out.println("id_doc = " + result1.get(i).getDocument().getId());
+            System.out.println(result1.get(i).getDocument().getContent());
+        }
+
+    }
+}
