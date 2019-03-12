584b2777116b4b72b5edfa91dda99fb2022aad54
xanadila <>
2019-03-12 23:32:37 +0700

    update

--- DocRetrieval/src/test/testTFIDF8.java
+++ DocRetrieval/src/test/testTFIDF8.java
@@ -16,9 +16,10 @@ import model.Term;
  * @author admin
  */
 public class testTFIDF8 {
+
     public static void main(String[] args) {
         // seting dokumen
-        Document doc1 = new Document(1, "Shipment of gold damaged in a fire");
+        Document doc1 = new Document(1, "shipment of gold damaged in a fire");
         Document doc2 = new Document(2, "delivery of silver arrived in a silver truck");
         Document doc3 = new Document(3, "shipment of gold arrived in a truck");
 
@@ -31,43 +32,12 @@ public class testTFIDF8 {
 
         // panggil fungsi search
         index.makeDictionaryWithTermNumber();
-        ArrayList<Term> result = index.getDictionary();
-        // tampilkan isi document dan id-nya
-        for (int i = 0; i < result.size(); i++) {
-            System.out.println("Term = " + result.get(i).getTerm()
-                    + ",numberOfDocument = " + result.get(i).getNumberOfDocument());
-            ArrayList<Posting> tempPosting = result.get(i).getPostingList();
-            for (int j = 0; j < tempPosting.size(); j++) {
-                System.out.println("id_doc = " + tempPosting.get(j).getDocument().getId()
-                        + ", numberofTerm = " + tempPosting.get(j).getNumberOfTerm());
-            }
-        }
 
-        // number of document
-        String tempString = "silver";
-        int result2 = index.getDocumentFrequency(tempString);
-        System.out.println("Number of Doc with " + tempString + " is " + result2);
+        String query = "gold silver truck";
 
-        // idf
-        String tempString1 = "silver";
-        double result3 = index.getInverseDocumentFrequency(tempString1);
-        System.out.println("IDF of " + tempString1 + " is " + result3);
+        ArrayList<Posting> p1 = index.makeQueryTFIDF(query);
+        ArrayList<Posting> p2 = index.makeTFIDF(2);
 
-        // tf
-        // idf
-        String tempString2 = "truck";
-        int idDoc = 2;
-        int result4 = index.getTermFrequency(tempString2, idDoc);
-        System.out.println("TF of " + tempString2 + " in idDoc = " + idDoc + " is " + result4);
-
-        // make arraylist of TFIDF
-        idDoc = 1;
-        ArrayList<Posting> tempDocWeight = index.makeTFIDF(idDoc);
-        for (int i = 0; i < tempDocWeight.size(); i++) {
-            Posting tempPost = tempDocWeight.get(i);
-            System.out.println("term= " + tempPost.getTerm()
-                    + ", tf = " + tempPost.getNumberOfTerm()
-                    + ", weight= " + tempPost.getWeight());
-        }
+        System.out.println(index.getInnerProduct(p1, p2));
     }
 }
