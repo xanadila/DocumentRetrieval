6540ab5ffb1d493be2bb6f0654d35f6c8ded521f
xanadila <>
2019-05-08 10:53:30 +0700

    update Posting

--- DocRetrieval/src/model/Posting.java
+++ DocRetrieval/src/model/Posting.java
@@ -104,4 +104,8 @@ public class Posting implements Comparable<Posting> {
         this.weight = weight;
     }
 
+    @Override
+    public String toString() {
+        return "Posting{" + "term=" + term + ", numberOfTerm=" + numberOfTerm + ", weight=" + weight + '}';
+    }
 }
