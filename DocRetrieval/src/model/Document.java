20c5029f4c574dfc59294d6199b8ebb7b8ba6e16
xanadila <>
2019-05-08 10:53:11 +0700

    update Document

--- DocRetrieval/src/model/Document.java
+++ DocRetrieval/src/model/Document.java
@@ -226,18 +226,18 @@ public class Document implements Comparable<Document> {
     }
 
     public void IndonesiaStemming() {
-        String text = content;
-
         Version matchVersion = Version.LUCENE_7_7_0; // Substitute desired Lucene version for XY
         Analyzer analyzer = new IndonesianAnalyzer();
         analyzer.setVersion(matchVersion);
+        // ambil stopwords
+        CharArraySet stopWords = IndonesianAnalyzer.getDefaultStopSet();
         // buat token
         TokenStream tokenStream = analyzer.tokenStream(
                 "myField",
-                new StringReader(text.trim()));
-        // stemming
-        tokenStream = new IndonesianStemFilter(tokenStream);
-
+                new StringReader(realContent.trim()));
+        // buang stop word
+        tokenStream = new StopFilter(tokenStream, stopWords);
+        // buat string baru tanpa stopword
         StringBuilder sb = new StringBuilder();
         CharTermAttribute charTermAttribute = tokenStream.addAttribute(CharTermAttribute.class);
         try {
